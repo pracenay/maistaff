0f9ee92dc9990cc98db96835ef8d27550b4bbfad
yanecarp <zrevkov@gmail.com>
2022-03-11 21:10:33 +0200

    responsive navbar

--- #src/js/script.js
+++ #src/js/script.js
@@ -3,8 +3,10 @@ window.addEventListener('scroll', () => {
 	window.scrollY > 0 ? nav.classList.add('scroll') : nav.classList.remove('scroll');
 });
 
-const navBtn = document.querySelector('.navigation__button');
-
+const navBtn = document.querySelector('.burger');
+const mobNav = document.querySelector('.mob-nav');
 navBtn.addEventListener('click', () => {
-	navBtn.classList.add('open');
+	navBtn.classList.toggle('open');
+	mobNav.classList.toggle('active');
+	mobNav.classList.contains('active') ? navBtn.classList.add('black') : navBtn.classList.remove('black');
 });
