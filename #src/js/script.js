0d96310b6411dd734aa4c2f9c7bb738962c986d7
yanecarp <zrevkov@gmail.com>
2022-03-23 20:00:51 +0200

    add faq page

--- #src/js/script.js
+++ #src/js/script.js
@@ -69,62 +69,11 @@ function initMap() {
 	});
 }
 
-$('select').each(function () {
-	var $this = $(this),
-		numberOfOptions = $(this).children('option').length;
-
-	$this.addClass('select-hidden');
-	$this.wrap('<div class="select"></div>');
-	$this.after('<div class="select-styled"></div>');
-
-	var $styledSelect = $this.next('div.select-styled');
-	$styledSelect.text($this.children('option').eq(0).text());
-
-	var $list = $('<ul />', {
-		class: 'select-options',
-	}).insertAfter($styledSelect);
-
-	for (var i = 0; i < numberOfOptions; i++) {
-		$('<li />', {
-			text: $this.children('option').eq(i).text(),
-			rel: $this.children('option').eq(i).val(),
-		}).appendTo($list);
-		//if ($this.children('option').eq(i).is(':selected')){
-		//  $('li[rel="' + $this.children('option').eq(i).val() + '"]').addClass('is-selected')
-		//}
-	}
-
-	var $listItems = $list.children('li');
-
-	$styledSelect.click(function (e) {
-		e.stopPropagation();
-		$('div.select-styled.active')
-			.not(this)
-			.each(function () {
-				$(this).removeClass('active').next('ul.select-options').hide();
-			});
-		$(this).toggleClass('active').next('ul.select-options').toggle();
-	});
-
-	$listItems.click(function (e) {
-		e.stopPropagation();
-		$styledSelect.text($(this).text()).removeClass('active');
-		$this.val($(this).attr('rel'));
-		$list.hide();
-		//console.log($this.val());
-	});
-
-	$(document).click(function () {
-		$styledSelect.removeClass('active');
-		$list.hide();
-	});
-});
-
 const modalWindow = document.querySelector('.modal');
 const closeModal = document.querySelector('.modal-close-btn');
 const btnsPublish = document.querySelectorAll('.data__control-btns button');
 
-modalWindow.addEventListener('click', (e) => {
+modalWindow?.addEventListener('click', (e) => {
 	if (e.target.classList.contains('modal')) {
 		modalWindow.classList.remove('active');
 	}
@@ -135,6 +84,27 @@ btnsPublish.forEach((btn) => {
 	});
 });
 
-closeModal.addEventListener('click', () => {
+closeModal?.addEventListener('click', () => {
 	modalWindow.classList.remove('active');
+});
+
+const accordionItemHeaders = document.querySelectorAll('.faq__accordion-item-header');
+
+accordionItemHeaders.forEach((accordionItemHeader) => {
+	accordionItemHeader.addEventListener('click', (event) => {
+		const currentlyActiveAccordionItemHeader = document.querySelector('.faq__accordion-item-header.active');
+		if (currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader) {
+			currentlyActiveAccordionItemHeader.classList.toggle('active');
+			currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+		}
+		console.log(accordionItemHeader);
+
+		accordionItemHeader.classList.toggle('active');
+		const accordionItemBody = accordionItemHeader.nextElementSibling;
+		if (accordionItemHeader.classList.contains('active')) {
+			accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + 'px';
+		} else {
+			accordionItemBody.style.maxHeight = 0;
+		}
+	});
 });
